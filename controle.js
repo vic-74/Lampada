1ae125ffe6630559b60ca23d024ac0f16770e9c9
vic-74 <victormirandafernandes574@gmail.com>
2025-08-20 12:13:41 -0300

    Add files via upload

---
+++ controle.js
@@ -0,0 +1,17 @@
+
+function alterarNome(){
+    var n = document.getElementById("nome");
+    n.innerHTML =`victor`
+}
+   
+
+
+function on(){
+    document.getElementById("lamp").src = "on.jpg"
+}
+function off(){
+    document.getElementById("lamp").src = "off.jpg"
+}
+function broken(){
+    document.getElementById("lamp").src = "broken.jpg"
+}
